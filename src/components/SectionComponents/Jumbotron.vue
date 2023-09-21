d6e6962b2e2ae0a6bd52d390eb24d5a36773a169
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-09-21 18:26:29 +0300

    style: responsive jumbotron

--- src/components/SectionComponents/Jumbotron.vue
+++ src/components/SectionComponents/Jumbotron.vue
@@ -55,6 +55,10 @@ export default {
         <div><em>{{ jumboImage.textSmall }} </em></div>
       </div>
     </div>
+    <div class="svgs">
+      <div class="svg-cart"><img src="../../assets/svg/svg-6.svg" class="cart"></div>
+      <div><img src="../../assets/svg/svg-7.svg" class="donut"></div>
+    </div>
 </div>
 
 
@@ -79,11 +83,63 @@ export default {
     
     font-size: 1.3rem;
   }
+
+  .svgs {
+    div {
+      img {
+      position: absolute;
+      right: 0;
+      width: 35px;
+      height: 35px;
+      padding: .5rem;
+    }
+    .cart {
+      top: 15%;
+      background-color: white;
+    }
+
+    .donut {
+      top: 10%;
+      background-color: red;
+      filter:sepia(46%) hue-rotate(356deg) saturate(193%) contrast(1) brightness(4.5)
+      }
+    }   
+  }
 }
 .thirdImageHover:hover .illustration {
   display: block;
   
 }
+
+@media screen and (max-width:768px) {
+  .jumbotron {
+    .illustration {
+      left: 50%;
+      bottom: 50%;
+
+      padding: .5rem 1rem;;
+      
+      font-size: 1rem;
+    }
+  .svgs {
+    div {
+      img {
+      width: 30px;
+      height: 30px;
+      }
+      .cart {
+        top: 36%;
+      }
+
+      .donut {
+        top: 40%;
+      }
+   }
+  }
+}
+
+}
+
 
 
 </style>
